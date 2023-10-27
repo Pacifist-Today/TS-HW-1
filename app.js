ba6a9033910d04ed880ce9fcac1abe32396f27f2
Pacifist <desmondmorales92@gmail.com>
2023-10-27 18:33:21 +0300

    all typezation

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var School = /** @class */ (function () {
 }());
 var Direction = /** @class */ (function () {
     function Direction(name) {
-        // если levels это градация классов, например от 1 по 4, то это четырёхмерный массив данных
-        // где levels включает в себя массив данных groups содержащий в себе массив каждой группы _students,
-        // в котором массив данных каждого студента
         this.levels = [];
         this._name = name;
     }
@@ -29,8 +26,6 @@ var Direction = /** @class */ (function () {
 }());
 var Level = /** @class */ (function () {
     function Level(name, program) {
-        // groups это трёхмерный массив содержащий в себе массив каждой группы _students,
-        // в котором массив данных каждого студента
         this.groups = [];
         this.name = name;
         this._program = program;
@@ -50,14 +45,12 @@ var Level = /** @class */ (function () {
         configurable: true
     });
     Level.prototype.addGroup = function (group) {
-        // group это двумерный массив, в котором вложены массивы данных студентов
         this.groups.push(group);
     };
     return Level;
 }());
 var Group = /** @class */ (function () {
     function Group(directionName, levelName) {
-        // _students это двумерный массив содержащий в себе массив данных каждого студента
         this._students = [];
         this.directionName = directionName;
         this.levelName = levelName;
@@ -70,7 +63,6 @@ var Group = /** @class */ (function () {
         configurable: true
     });
     Group.prototype.addStudent = function (student) {
-        // student это одномерный массив данных каждого студента
         this._students.push(student);
     };
     Group.prototype.showPerformance = function () {
@@ -123,3 +115,137 @@ var Student = /** @class */ (function () {
     };
     return Student;
 }());
+// class School {
+//     // Если directions это условная "Специализация", то мы получаем пятимерный массив данных
+//     // levels это градация классов, например от 1 по 4, то это четырёхмерный массив данных
+//     // где levels включает в себя массив данных
+//     // groups содержащий в себе массив каждой группы _students,
+//     // в котором массив данных каждого студента
+//
+//     directions: [][][][][] = [];
+//
+//     addDirection(direction: []): void {
+//         this.directions.push(direction);
+//     }
+// }
+//
+// class Direction {
+//     // если levels это градация классов, например от 1 по 4, то это четырёхмерный массив данных
+//     // где levels включает в себя массив данных
+//     // groups содержащий в себе массив каждой группы _students,
+//     // в котором массив данных каждого студента
+//     levels: [][][][] = [];
+//
+//     get name(): string {
+//         return this._name;
+//     }
+//
+//     constructor(name: string) {
+//         this._name = name;
+//     }
+//
+//     addLevel(level: [][][]): void {
+//         this.levels.push(level);
+//     }
+// }
+//
+// class Level {
+//     // groups это трёхмерный массив содержащий в себе массив каждой группы _students,
+//     // в котором массив данных каждого студента
+//
+//     groups: [][][] = [];
+//
+//     constructor(name: string, program: string) {
+//         this.name = name;
+//         this._program = program;
+//     }
+//
+//     get name(): string {
+//         return this._name;
+//     }
+//
+//     get program(): string {
+//         return this._program;
+//     }
+//
+//     addGroup(group: [][]): void {
+//         // group это двумерный массив, в котором вложены массивы данных студентов
+//         this.groups.push(group);
+//     }
+// }
+//
+// class Group {
+//     // _students это двумерный массив содержащий в себе массив данных каждого студента
+//
+//     _students: [][] = [];
+//
+//     get students(): [][] {
+//         return this._students;
+//     }
+//
+//     constructor(directionName: string, levelName: string) {
+//         this.directionName = directionName;
+//         this.levelName = levelName;
+//     }
+//
+//     addStudent(student: []): void {
+//         // student это одномерный массив данных каждого студента
+//
+//         this._students.push(student);
+//     }
+//
+//     showPerformance(): [][] {
+//         const sortedStudents: [] = this.students.toSorted(
+//             (a:[], b:[]) => b.getPerformanceRating() - a.getPerformanceRating()
+//         );
+//
+//         return sortedStudents;
+//     }
+// }
+//
+// class Student {
+//     grades: {[computedProperty: string]: number} = {};
+//     attendance: boolean[] = [];
+//
+//     constructor(firstName: string, lastName: string, birthYear: number) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.birthYear = birthYear;
+//     }
+//
+//     get fullName(): string {
+//         return `${this.lastName} ${this.firstName}`;
+//     }
+//
+//     set fullName(value: string) {
+//         [this.lastName, this.firstName] = value.split(" ");
+//     }
+//
+//     get age(): number {
+//         return new Date().getFullYear() - this.birthYear;
+//     }
+//
+//     setGrade(subject: string, grade: number): void {
+//         this.grades[subject] = grade;
+//     }
+//
+//     markAttendance(present: boolean): void {
+//         this.attendance.push(present);
+//     }
+//
+//     getPerformanceRating(): number {
+//         const gradeValues = Object.values(this.grades);
+//
+//         if (gradeValues.length === 0) return 0;
+//
+//         const averageGrade =
+//             gradeValues.reduce((sum: number, grade: number) => sum + grade, 0) / gradeValues.length;
+//
+//         const attendancePercentage: number =
+//             (this.attendance.filter((present: boolean) => present).length /
+//                 this.attendance.length) *
+//             100;
+//
+//         return (averageGrade + attendancePercentage) / 2;
+//     }
+// }
--- app.ts
+++ app.ts
@@ -1,9 +1,4 @@
 class School {
-    // Если directions это условная "Специализация", то мы получаем пятимерный массив данных
-    // levels это градация классов, например от 1 по 4, то это четырёхмерный массив данных
-    // где levels включает в себя массив данных
-    // groups содержащий в себе массив каждой группы _students,
-    // в котором массив данных каждого студента
 
     directions: [][][][][] = [];
 
@@ -13,10 +8,7 @@ class School {
 }
 
 class Direction {
-    // если levels это градация классов, например от 1 по 4, то это четырёхмерный массив данных
-    // где levels включает в себя массив данных
-    // groups содержащий в себе массив каждой группы _students,
-    // в котором массив данных каждого студента
+
     levels: [][][][] = [];
 
     get name(): string {
@@ -27,14 +19,12 @@ class Direction {
         this._name = name;
     }
 
-    addLevel(level: [][][]) {
+    addLevel(level: [][][]): void {
         this.levels.push(level);
     }
 }
 
 class Level {
-    // groups это трёхмерный массив содержащий в себе массив каждой группы _students,
-    // в котором массив данных каждого студента
 
     groups: [][][] = [];
 
@@ -52,13 +42,11 @@ class Level {
     }
 
     addGroup(group: [][]): void {
-        // group это двумерный массив, в котором вложены массивы данных студентов
         this.groups.push(group);
     }
 }
 
 class Group {
-    // _students это двумерный массив содержащий в себе массив данных каждого студента
 
     _students: [][] = [];
 
@@ -72,16 +60,13 @@ class Group {
     }
 
     addStudent(student: []): void {
-        // student это одномерный массив данных каждого студента
-
         this._students.push(student);
     }
 
-    showPerformance(): [][] {
-        const sortedStudents = this.students.toSorted(
+    showPerformance(): [] {
+        const sortedStudents: [] = this.students.toSorted(
             (a:[], b:[]) => b.getPerformanceRating() - a.getPerformanceRating()
         );
-
         return sortedStudents;
     }
 }
@@ -108,7 +93,7 @@ class Student {
         return new Date().getFullYear() - this.birthYear;
     }
 
-    setGrade(subject: string, grade: number) {
+    setGrade(subject: string, grade: number): void {
         this.grades[subject] = grade;
     }
 
@@ -132,3 +117,138 @@ class Student {
         return (averageGrade + attendancePercentage) / 2;
     }
 }
+
+// class School {
+//     // Если directions это условная "Специализация", то мы получаем пятимерный массив данных
+//     // levels это градация классов, например от 1 по 4, то это четырёхмерный массив данных
+//     // где levels включает в себя массив данных
+//     // groups содержащий в себе массив каждой группы _students,
+//     // в котором массив данных каждого студента
+//
+//     directions: [][][][][] = [];
+//
+//     addDirection(direction: []): void {
+//         this.directions.push(direction);
+//     }
+// }
+//
+// class Direction {
+//     // если levels это градация классов, например от 1 по 4, то это четырёхмерный массив данных
+//     // где levels включает в себя массив данных
+//     // groups содержащий в себе массив каждой группы _students,
+//     // в котором массив данных каждого студента
+//     levels: [][][][] = [];
+//
+//     get name(): string {
+//         return this._name;
+//     }
+//
+//     constructor(name: string) {
+//         this._name = name;
+//     }
+//
+//     addLevel(level: [][][]): void {
+//         this.levels.push(level);
+//     }
+// }
+//
+// class Level {
+//     // groups это трёхмерный массив содержащий в себе массив каждой группы _students,
+//     // в котором массив данных каждого студента
+//
+//     groups: [][][] = [];
+//
+//     constructor(name: string, program: string) {
+//         this.name = name;
+//         this._program = program;
+//     }
+//
+//     get name(): string {
+//         return this._name;
+//     }
+//
+//     get program(): string {
+//         return this._program;
+//     }
+//
+//     addGroup(group: [][]): void {
+//         // group это двумерный массив, в котором вложены массивы данных студентов
+//         this.groups.push(group);
+//     }
+// }
+//
+// class Group {
+//     // _students это двумерный массив содержащий в себе массив данных каждого студента
+//
+//     _students: [][] = [];
+//
+//     get students(): [][] {
+//         return this._students;
+//     }
+//
+//     constructor(directionName: string, levelName: string) {
+//         this.directionName = directionName;
+//         this.levelName = levelName;
+//     }
+//
+//     addStudent(student: []): void {
+//         // student это одномерный массив данных каждого студента
+//
+//         this._students.push(student);
+//     }
+//
+//     showPerformance(): [][] {
+//         const sortedStudents: [] = this.students.toSorted(
+//             (a:[], b:[]) => b.getPerformanceRating() - a.getPerformanceRating()
+//         );
+//
+//         return sortedStudents;
+//     }
+// }
+//
+// class Student {
+//     grades: {[computedProperty: string]: number} = {};
+//     attendance: boolean[] = [];
+//
+//     constructor(firstName: string, lastName: string, birthYear: number) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.birthYear = birthYear;
+//     }
+//
+//     get fullName(): string {
+//         return `${this.lastName} ${this.firstName}`;
+//     }
+//
+//     set fullName(value: string) {
+//         [this.lastName, this.firstName] = value.split(" ");
+//     }
+//
+//     get age(): number {
+//         return new Date().getFullYear() - this.birthYear;
+//     }
+//
+//     setGrade(subject: string, grade: number): void {
+//         this.grades[subject] = grade;
+//     }
+//
+//     markAttendance(present: boolean): void {
+//         this.attendance.push(present);
+//     }
+//
+//     getPerformanceRating(): number {
+//         const gradeValues = Object.values(this.grades);
+//
+//         if (gradeValues.length === 0) return 0;
+//
+//         const averageGrade =
+//             gradeValues.reduce((sum: number, grade: number) => sum + grade, 0) / gradeValues.length;
+//
+//         const attendancePercentage: number =
+//             (this.attendance.filter((present: boolean) => present).length /
+//                 this.attendance.length) *
+//             100;
+//
+//         return (averageGrade + attendancePercentage) / 2;
+//     }
+// }
